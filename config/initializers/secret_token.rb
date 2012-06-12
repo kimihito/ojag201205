21a4d00253b4cdc6fd1f0b263fb9fe4bbfffe3f7
kimihito <tatsurotamashiro@gmail.com>
2012-06-13 07:34:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chumon::Application.config.secret_token = '3b3cdbacd1e2fba1aa400f1fa80d464252b4204cc2aa397db708f25945d2e0039129e547ebcf6ad44ac0af78e8eb68e650b0d6bcf9c5002646f29cdac300b159'
